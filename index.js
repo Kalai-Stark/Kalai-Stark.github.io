cd4bb5526b14c66276f69f9aa32701152c932c5e
kalai selvan <kalaiselvan.18.2@protosem.tech>
2018-11-22 00:25:33 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 const express = require('express'),
 app = express(),
 port = 3000;
-// app.use('/',function(res,req){
-//     res.end('Hello World');
-//     } )
-
-
-
-
-
-// var express = require('express')
-// var app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (res,req) {
